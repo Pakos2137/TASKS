bec69237ca0ff84c0044c51590248961550bcdec
Pakos2137 <michal.pakos@Onet.pl>
2023-05-16 22:08:47 +0200

    22.3

--- tasks/src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ tasks/src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -3,18 +3,23 @@ package com.crud.tasks.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
+import java.net.URI;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class CreatedTrelloCard {
     @JsonProperty("id")
     private String id;
 
     @JsonProperty("name")
     private String name;
 
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty("shortUrl")
+    private String shortUrl;
+
+    @JsonProperty("badges")
+    private BadgesDto badges;
+
 }
